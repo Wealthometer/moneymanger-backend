2e2f7eb2788eba822df6cec71963b83106fed2c6
Wealthometer <wealthhajoh87@gmail.com>
2025-11-18 03:22:07 +0100

    Create ProfileController.java

---
+++ src/main/java/in/project/moneymanager/controller/ProfileController.java
@@ -0,0 +1,56 @@
+package in.project.moneymanager.controller;
+
+import in.project.moneymanager.dto.AuthDTO;
+import in.project.moneymanager.dto.ProfileDTO;
+import in.project.moneymanager.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
+        ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+//        return ResponseEntity.ok(registeredProfile);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+    }
+
+    @GetMapping("/activate")
+    public ResponseEntity<String> activateProfile(@RequestParam String token) {
+        boolean isActivated = profileService.activateProfile(token);
+        if (isActivated) {
+            return ResponseEntity.ok("Profile activated successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Activation Token Not Found or already Used.");
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String,Object>> login (@RequestBody AuthDTO authDTO) {
+        try {
+            if (!profileService.isAccountActive(authDTO.getEmail())) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                        .body(Map.of(
+                                "message",
+                                "Account is not activated. Please activate your account First."
+                        ));
+            }
+            Map<String, Object> response = profileService.authenticateAndGenerateToken(authDTO);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                    "message",
+                    e.getMessage()
+            ));
+
+        }
+    }
+}
