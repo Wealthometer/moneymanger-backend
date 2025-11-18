1056e21f1328e90ece933405bfc2f0e31f7d1c0a
Wealthometer <wealthhajoh87@gmail.com>
2025-11-18 03:22:13 +0100

    Create ProfileDTO.java

---
+++ src/main/java/in/project/moneymanager/dto/ProfileDTO.java
@@ -0,0 +1,25 @@
+package in.project.moneymanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProfileDTO {
+
+    private Long id;
+    private String fullname;
+    private String email;
+    private String password;
+    private String profileImageUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+//    private Boolean isActive;
+//    private String activationToken;
+}
