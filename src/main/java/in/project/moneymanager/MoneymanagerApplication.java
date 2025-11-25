5078685b00c32397bf583791a646e7eb8d8f24a3
Wealthometer <wealthhajoh87@gmail.com>
2025-11-25 15:43:09 +0100

    Update MoneymanagerApplication.java

--- src/main/java/in/project/moneymanager/MoneymanagerApplication.java
+++ src/main/java/in/project/moneymanager/MoneymanagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoneymanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoneymanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoneymanagerApplication.class, args);
+    }
 
 }
