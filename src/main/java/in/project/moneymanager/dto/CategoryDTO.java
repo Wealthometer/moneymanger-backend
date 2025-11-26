8b5156b7b3301d00a9d3f92f0c0144edafab7da5
Wealthometer <wealthhajoh87@gmail.com>
2025-11-26 08:40:54 +0100

    Update CategoryDTO.java

--- src/main/java/in/project/moneymanager/dto/CategoryDTO.java
+++ src/main/java/in/project/moneymanager/dto/CategoryDTO.java
@@ -1,15 +1,24 @@
 package in.project.moneymanager.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CategoryDTO {
 
     private Long id;
-    private String profileId;
+    private Long profileId;
     private String name;
     private String icon;
     private String type;// e.g., "income" or "expense"
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    
+
 }
