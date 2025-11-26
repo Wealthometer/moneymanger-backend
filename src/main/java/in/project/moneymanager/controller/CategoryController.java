9ffd863e04bb2fda0634ead4e8c482170bb5228f
Wealthometer <wealthhajoh87@gmail.com>
2025-11-26 10:32:51 +0100

    Update CategoryController.java

--- src/main/java/in/project/moneymanager/controller/CategoryController.java
+++ src/main/java/in/project/moneymanager/controller/CategoryController.java
@@ -1,4 +1,25 @@
 package in.project.moneymanager.controller;
 
+import in.project.moneymanager.dto.CategoryDTO;
+import in.project.moneymanager.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
 }
