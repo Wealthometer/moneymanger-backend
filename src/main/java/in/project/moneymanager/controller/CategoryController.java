bf0fe43fe8a43cf83c5d4b22d26f385e47ef2f08
Wealthometer <wealthhajoh87@gmail.com>
2025-11-27 06:39:00 +0100

    Update CategoryController.java

--- src/main/java/in/project/moneymanager/controller/CategoryController.java
+++ src/main/java/in/project/moneymanager/controller/CategoryController.java
@@ -33,4 +33,10 @@ public class CategoryController {
         List<CategoryDTO> list = categoryService.getCategoriesByTypeForCurrentUser(type);
         return ResponseEntity.ok(list);
     }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId, @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryId, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
+    }
 }
