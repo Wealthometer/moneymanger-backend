f0a77e0d35799e0dbd1ce22ae628eb891ee85d48
Wealthometer <wealthhajoh87@gmail.com>
2025-11-29 18:11:51 +0100

    Update IncomeDTO.java

--- src/main/java/in/project/moneymanager/dto/IncomeDTO.java
+++ src/main/java/in/project/moneymanager/dto/IncomeDTO.java
@@ -1,9 +1,18 @@
 package in.project.moneymanager.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class IncomeDTO {
 
     private Long id;
