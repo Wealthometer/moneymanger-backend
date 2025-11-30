1667b9d42e4f06db5909b2dca24decc0e4c45d37
Wealthometer <wealthhajoh87@gmail.com>
2025-11-30 06:55:09 +0100

    Create IncomeService.java

---
+++ src/main/java/in/project/moneymanager/service/IncomeService.java
@@ -0,0 +1,47 @@
+package in.project.moneymanager.service;
+
+import in.project.moneymanager.dto.ExpenseDTO;
+import in.project.moneymanager.dto.IncomeDTO;
+import in.project.moneymanager.entity.CategoryEntity;
+import in.project.moneymanager.entity.ExpenseEntity;
+import in.project.moneymanager.entity.IncomeEntity;
+import in.project.moneymanager.entity.ProfileEntity;
+import in.project.moneymanager.repository.ExpenseRepository;
+import in.project.moneymanager.repository.IncomeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IncomeService {
+
+    private final CategoryService categoryService;
+    private final IncomeRepository incomeRepository;
+
+    // helper methods
+    private IncomeEntity toEntity(IncomeDTO dto, ProfileEntity profile, CategoryEntity category) {
+        return IncomeEntity.builder()
+                .name(dto.getName())
+                .icon(dto.getIcon())
+                .amount(dto.getAmount())
+                .date(dto.getDate())
+                .profile(profile)
+                .category(category)
+                .build();
+    }
+
+    private IncomeDTO toDTO(IncomeEntity entity) {
+        return IncomeDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .categoryId(entity.getCategory() != null ? entity.getCategory().getId() : null)
+                .categoryName(entity.getCategory() != null ? entity.getCategory().getName() : "N/A")
+                .amount(entity.getAmount())
+                .date(entity.getDate())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
+
+}
